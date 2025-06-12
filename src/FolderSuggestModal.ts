0f0af0c64a703f504b60b0623ea9712a9e8b65ca
James Sonneveld <jsonneveld2@hotmail.com>
2025-06-12 11:51:36 +1000

    Fix #9: Refactor 'FolderModelSuggest.ts' I was told that Obsidian's FuzzySuggestModel<T> implementation was exactly what I was trying to acheive with my implementation. Refactor code in 'FolderSuggestModel.ts' to use FuzzySuggestModel<T>.

--- src/FolderSuggestModal.ts
+++ src/FolderSuggestModal.ts
@@ -1,4 +1,4 @@
-import { SuggestModal, App, TFolder } from "obsidian";
+import { App, TFolder, FuzzySuggestModal } from "obsidian";
 
 type StringCallback = (str: string) => void
 
@@ -9,65 +9,25 @@ type StringCallback = (str: string) => void
 * @param {App} app The instance of the obsidian app.
 * @param {StringCallback} callback A callback function that is called on items in the SuggestModal when a user makes a selection.
 */
-export class FolderSuggestModal extends SuggestModal<string> {
+export class FolderSuggestModal extends FuzzySuggestModal<string> {
+	
 	private callback: StringCallback
-	private query: string = ''
 
 	constructor(app: App, callback: StringCallback) {
-		super(app);
-		this.callback = callback;
+		super(app)
+		this.callback = callback
 	}
 
-	/**
-	* @param {string} query query is the string input from the user when typing in the SuggestModal input.
-	* @returns {string[] | Promise<string[]>} Returns an array of folder paths.
-	*/
-	getSuggestions(query: string): string[] | Promise<string[]> {
-		this.query = query
-		const folders: string[] = this.getFolderNames()
-
-		return folders.filter((item) => {
-			return item.includes(query)
-		})
-	}
-
-	/**
-	* As well as rendering each string value, 'renderSuggestion' also creates a regular expression
-	* to replace the queried term with the same query just with the inline HTML tag 'span' inserted inbetween.
-	* The span is given the class name 'query-highlight' found in 'styles.css' to provide a yellow highlight to
-	* the text.
-	* @param {string} value A string value to display to the user.
-	* @param {HTMLElement} el The HTML element that can be used to display 'value' in the FolderSuggestModal. 
-	*/
-	renderSuggestion(value: string, el: HTMLElement): void {
-		el.setText(value)
-		// Only replace when query string is greater than 0 to prevent
-		// 'replace' from inserting <span></span> between each character.
-		if (this.query.length > 0) {
-			const highlightedStr: string = this.applyStyleToSubstringWithin(value, this.query, 'query-highlight');
-			el.createSpan(highlightedStr)
-		}
+	getItems(): string[] {
+		return this.getFolderNames()
 	}
 	
-	/**
-	* @param {string} item 
-	* @param {MouseEvent | KeyboardEvent} _evt
-	*/
-	onChooseSuggestion(item: string, _evt: MouseEvent | KeyboardEvent): void {
+	getItemText(item: string): string {
+		return item
+	}
+	
+	onChooseItem(item: string, evt: MouseEvent | KeyboardEvent): void {
 		this.callback(item)
-	}
-	
-	/**
-	* Takes a string value and attempts to match a substring within it's contents.
-	* Applies a HTML <span> element across the substring with the supplied CSS class name.
-	* @param str Input string to search for substring within.
-	* @param subStr Substring to search for.
-	* @param cssClassName CSS class name to apply to found substring.
-	* @returns {string} Returns the modified string with the span tag inserted between the substring found in the original string.
-	*/
-	applyStyleToSubstringWithin(str: string, subStr: string, cssClassName: string): string {
-		const match: RegExp = new RegExp(`${subStr}`, 'g');
-		return str.replace(match, `<span class="${cssClassName}">${subStr}</span>`);
 	}
 
 	/** 
